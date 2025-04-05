34a161173aefbc67d191dadf5f24d87bc6fc7c29
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-04-05 16:13:31 +0200

    add basic infra for maven projects

---
+++ data-scraper/src/test/java/group_05/ase/data_scraper/DataScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package group_05.ase.data_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
