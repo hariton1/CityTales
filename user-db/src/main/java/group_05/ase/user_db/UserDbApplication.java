32b4e53a1109a9ab49318c60f601a23410d9b08b
Aficionado29 <ivan1234@centrum.sk>
2025-04-09 19:46:33 +0200

    basic UserDB application

---
+++ user-db/src/main/java/group_05/ase/user_db/UserDbApplication.java
@@ -0,0 +1,13 @@
+package group_05.ase.user_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDbApplication.class, args);
+	}
+
+}
---
+++ user-db/src/test/java/group_05/ase/user_db/UserDbApplicationTests.java
@@ -0,0 +1,13 @@
+package group_05.ase.user_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
