122534c3c9f7f150f31463b956d782e1ebe151fa
Ivan Lichner <e1226385@student.tuwien.ac.at>
2025-04-10 22:47:45 +0200

    connection to Supabase is fixed and application is runable

--- user-db/src/main/java/group_05/ase/user_db/endpoints/TestController.java
+++ user-db/src/main/java/group_05/ase/user_db/endpoints/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String testEndpoint() {
-        return "API is working!";
+        return "Connection to PostgreDB is working!";
     }
 }
