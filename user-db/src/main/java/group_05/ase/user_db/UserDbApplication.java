2de525b3fb66e1d4c422d42b7682ed1a3ab1ca45
Ivan Lichner <e1226385@student.tuwien.ac.at>
2025-04-23 18:32:47 +0200

    retrieval of content of INTEREST table added

--- user-db/src/main/java/group_05/ase/user_db/UserDbApplication.java
+++ user-db/src/main/java/group_05/ase/user_db/UserDbApplication.java
@@ -2,8 +2,10 @@ package group_05.ase.user_db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class UserDbApplication {
 
 	public static void main(String[] args) {
--- user-db/src/main/java/group_05/ase/user_db/endpoints/TestController.java
+++ user-db/src/main/java/group_05/ase/user_db/endpoints/TestController.java
@@ -1,12 +1,36 @@
 package group_05.ase.user_db.endpoints;
 
+import group_05.ase.user_db.services.InterestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class TestController {
+
+    private final InterestService interestService;
+
+    @Autowired
+    public TestController(InterestService interestService) {
+        this.interestService = interestService;
+    }
+
     @GetMapping("/test")
     public String testEndpoint() {
         return "Connection to PostgreDB is working!";
     }
+
+    @GetMapping("/interests")
+    @ResponseStatus(HttpStatus.OK)
+    public String getAllInterests() {
+        try {
+            return this.interestService.getAllInterests().toString();
+        } catch (Exception e) {
+            return "An internal server error occurred => " + e.getMessage();
+        }
+    }
+
 }
