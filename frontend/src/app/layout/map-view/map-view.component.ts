e422e971e55f8f7a19a5f1660ccc9fb611886bad
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-04-30 00:20:29 +0200

    add Google Maps integration for map-view

--- frontend/src/app/layout/map-view/map-view.component.ts
+++ frontend/src/app/layout/map-view/map-view.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {GoogleMap, MapMarker} from '@angular/google-maps';
 
 @Component({
   selector: 'app-map-view',
-  imports: [],
+  imports: [
+    GoogleMap,
+    MapMarker
+  ],
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.scss'
 })
 export class MapViewComponent {
+  center: google.maps.LatLngLiteral = { lat: 48.19865798950195, lng: 16.3714542388916 };
+  zoom = 12;
 
+  markerPosition: google.maps.LatLngLiteral = this.center;
 }
