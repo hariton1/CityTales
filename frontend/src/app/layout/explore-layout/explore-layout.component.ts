7bbbe93467afd95e07c086cfe8b99836dbd65143
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-04-29 23:28:06 +0200

    create explore-layout component

---
+++ frontend/src/app/layout/explore-layout/explore-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-layout',
+  imports: [],
+  templateUrl: './explore-layout.component.html',
+  styleUrl: './explore-layout.component.scss'
+})
+export class ExploreLayoutComponent {
+
+}
