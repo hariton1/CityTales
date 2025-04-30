123c317d1be86573d4066a236c1b2f5e55005d23
ChristophWinkler <christophwinkler2@gmail.com>
2025-04-30 02:24:38 +0200

    established connection between frontend and userDB

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TuiRoot, TuiAlertService, TuiButton} from '@taiga-ui/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,17 @@ import {TuiRoot, TuiAlertService, TuiButton} from '@taiga-ui/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
 
   private readonly alerts = inject(TuiAlertService);
+  private readonly httpClient: HttpClient = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.httpClient.get('http://localhost:8090/userInterests', {responseType: "text"}).subscribe(buffer => {
+      console.log(buffer);
+    });
+  }
 
   protected showNotification(): void {
     console.log('show notification');
