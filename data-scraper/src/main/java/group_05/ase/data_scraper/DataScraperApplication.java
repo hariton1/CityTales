d911fd7373c9bd6048e9b68cc17a201efc6de226
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-02 23:15:43 +0200

    adapt sample data set config

--- data-scraper/src/main/java/group_05/ase/data_scraper/DataScraperApplication.java
+++ data-scraper/src/main/java/group_05/ase/data_scraper/DataScraperApplication.java
@@ -13,9 +13,13 @@ public class DataScraperApplication {
 		ApplicationContext applicationContext = SpringApplication.run(DataScraperApplication.class, args);
 		WikiDataScraperService service = applicationContext.getBean(WikiDataScraperService.class);
 
-		String continueToken = "0|387470"; //Karlskirche
-		// Sample Dataset: 200 entries
-		service.batchSearch(500, 4,"");
+
+		// String continueToken = "0|387470"; //Karlskirche
+		String continueToken = "0|697568"; // Arsenal
+		// Sample Dataset: 300 entries - max number of nodes in neo4j-ui
+		service.batchSearch(100, 1,"");
+		service.batchSearch(200, 1,continueToken);
+
 		service.upsertLinkages();
 	}
 }
