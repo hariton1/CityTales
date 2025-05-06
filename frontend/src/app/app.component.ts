6239a8fb8e5fe7ecb353d9ef8686699f28de68f9
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-06 15:08:46 +0200

    Add routing for Explore, User List  - create a User List component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import {Component} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet} from '@angular/router';
 import {TuiRoot} from '@taiga-ui/core';
 import {HeaderComponent} from './core/header/header.component';
-import {ExploreLayoutComponent} from './layout/explore-layout/explore-layout.component';
 import {MobileNavigationComponent} from './core/mobile-navigation/mobile-navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRoot, HeaderComponent, ExploreLayoutComponent, MobileNavigationComponent],
+  imports: [RouterOutlet, TuiRoot, HeaderComponent, MobileNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {UserListComponent} from './user-management/pages/user-list/user-list.component';
+import {ExploreLayoutComponent} from './layout/explore-layout/explore-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'admin/user-list', component: UserListComponent },
+  { path: 'explore', component: ExploreLayoutComponent },
+  { path: '', redirectTo: '/explore', pathMatch: 'full' } //default page, to be changed
+];
