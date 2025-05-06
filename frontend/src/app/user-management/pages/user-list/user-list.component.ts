831e690a359f6d0f679bd6a1daee6d1088a3bf02
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-06 18:17:57 +0200

    create a table for admin user management

--- frontend/src/app/user-management/pages/user-list/user-list.component.ts
+++ frontend/src/app/user-management/pages/user-list/user-list.component.ts
@@ -1,11 +1,89 @@
 import { Component } from '@angular/core';
+import {
+  TuiAutoColorPipe,
+  TuiButton,
+  TuiDropdownDirective, TuiDropdownOpen,
+  TuiDropdownOptionsDirective,
+  TuiInitialsPipe,
+  TuiLink, TuiTitle
+} from '@taiga-ui/core';
+import {TuiAvatar, TuiBadge, TuiItemsWithMoreComponent, TuiMore, TuiStatus} from '@taiga-ui/kit';
+import {TuiCell} from '@taiga-ui/layout';
+import {NgForOf, NgIf} from '@angular/common';
+import {TuiTable} from '@taiga-ui/addon-table';
+import {TuiItem} from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [
+    TuiInitialsPipe,
+    TuiAutoColorPipe,
+    TuiAvatar,
+    TuiCell,
+    TuiStatus,
+    TuiItemsWithMoreComponent,
+    TuiBadge,
+    TuiDropdownDirective,
+    TuiLink,
+    NgForOf,
+    NgIf,
+    TuiTable,
+    TuiItem,
+    TuiButton,
+    TuiMore,
+    TuiDropdownOptionsDirective,
+    TuiDropdownOpen,
+    TuiTitle
+  ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
-
+  protected readonly data = [
+    {
+      role: {
+        roleName: 'Moderator'
+      },
+      userData: {
+        name: 'John Cleese',
+        email: 'silly@walk.uk',
+      },
+      status: {
+        value: 'Active',
+        color: 'var(--tui-status-positive)',
+      },
+      interests: ['Some', 'items', 'displayed', 'here', 'and', 'can', 'overflow'],
+      createdAt: '2021-01-01'
+    },
+    {
+      role: {
+        roleName: 'User'
+      },
+      userData: {
+        name: 'Eric Idle',
+        email: 'cool@dude.com',
+      },
+      status: {
+        value: 'Blocked',
+        color: 'var(--tui-status-negative)',
+      },
+      interests: ['One', 'Item'],
+      createdAt: '2024-02-23'
+    },
+    {
+      role: {
+        roleName: 'Contributor'
+      },
+      userData: {
+        name: 'Michael Palin',
+        email: 'its@man.com',
+      },
+      status: {
+        value: 'Active',
+        color: 'var(--tui-status-positive)',
+      },
+      interests: [],
+      createdAt: '2024-07-16'
+    },
+  ];
 }
