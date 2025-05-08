455bf6fa508b99ad544b832e95bfd4c918af7354
Bianca <bianca.traeger@tuwien.ac.at>
2025-05-08 15:51:05 +0200

    add securityconfig, db connection valid of public shema but not for auth

--- authentification/src/main/java/group_05/ase/auth/RegisterRequest.java
+++ authentification/src/main/java/group_05/ase/auth/RegisterRequest.java
@@ -1,10 +1,10 @@
+
 package group_05.ase.auth;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
     private String email;
     private String password;
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 }
