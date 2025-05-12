0f3b3a713a52a8f407eddee74ce40d2673a0e4f8
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-12 16:18:28 +0200

    create sign-up component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {UserListComponent} from './user-management/pages/user-list/user-list.component';
 import {ExploreLayoutComponent} from './layout/explore-layout/explore-layout.component';
 import {LoginComponent} from './user-management/pages/login/login.component';
+import {SignUpComponent} from './user-management/pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: 'admin/user-list', component: UserListComponent },
   { path: 'explore', component: ExploreLayoutComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignUpComponent },
   { path: '', redirectTo: '/explore', pathMatch: 'full' } //default page, to be changed
 ];
