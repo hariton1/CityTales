dead52c960d70ae34d248df6fcc64466b6130abf
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-12 22:17:41 +0200

    change route path name for user list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import {LoginComponent} from './user-management/pages/login/login.component';
 import {SignUpComponent} from './user-management/pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
-  { path: 'admin/user-list', component: UserListComponent },
+  { path: 'admin/users', component: UserListComponent },
   { path: 'explore', component: ExploreLayoutComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: SignUpComponent },
