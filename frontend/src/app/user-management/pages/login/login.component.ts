13f6b6fae1c35f43b1c161b0e256b49b6c039c82
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-12 16:12:15 +0200

    create ui skeleton for Login page

--- frontend/src/app/user-management/pages/login/login.component.ts
+++ frontend/src/app/user-management/pages/login/login.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import {TuiButton, TuiError, TuiLabel, TuiTextfieldComponent, TuiTextfieldDirective, TuiTitle, TuiIcon} from '@taiga-ui/core';
+import {TuiForm, TuiHeader} from '@taiga-ui/layout';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {TuiFieldErrorPipe, TuiPassword, TuiTooltip} from '@taiga-ui/kit';
+import {AsyncPipe} from '@angular/common';
+import {TuiInputModule} from '@taiga-ui/legacy';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    TuiButton,
+    TuiForm,
+    TuiTextfieldComponent,
+    TuiHeader,
+    TuiTitle,
+    TuiLabel,
+    ReactiveFormsModule,
+    TuiTextfieldDirective,
+    TuiError,
+    TuiFieldErrorPipe,
+    AsyncPipe,
+    TuiPassword,
+    TuiTooltip,
+    TuiIcon,
+    TuiInputModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  protected loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    passwordValue: new FormControl('', Validators.required),
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Form submitted with values:', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
 
 }
