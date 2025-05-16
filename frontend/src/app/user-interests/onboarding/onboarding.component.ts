a17c71083d359c70c10da73969798172e6ff13ac
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-16 19:18:32 +0200

    add interests select ui

--- frontend/src/app/user-interests/onboarding/onboarding.component.ts
+++ frontend/src/app/user-interests/onboarding/onboarding.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import {JsonPipe} from '@angular/common';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {TuiFilter} from '@taiga-ui/kit';
+import {UserService} from '../../services/user.service';
+
 
 @Component({
   selector: 'app-onboarding',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe,
+    ReactiveFormsModule,
+    TuiFilter
+  ],
   templateUrl: './onboarding.component.html',
-  styleUrl: './onboarding.component.scss'
+  styleUrl: './onboarding.component.scss',
 })
+
 export class OnboardingComponent {
+
+  constructor(private userService: UserService) {
+  }
+
+
+  protected readonly form = new FormGroup({
+    filters: new FormControl(['Food']),
+  });
+
+  protected readonly items = [
+    'News',
+    'Food',
+    'Clothes',
+    'Popular',
+    'Goods',
+    'Furniture',
+    'Tech',
+    'Building materials',
+  ];
 
 }
