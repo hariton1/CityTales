654c44191a2df74d16505655d5a79e2ff73bffb8
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-16 19:56:21 +0200

    create interest DTO

---
+++ frontend/src/app/dto/interest.dto.ts
@@ -0,0 +1,24 @@
+export class InterestDto {
+  private interestId: number;
+  private interestName: string;
+  private description: string;
+
+  constructor( interestId: number, interestName: string, description: string) {
+    this.interestId = interestId;
+    this.interestName = interestName;
+    this.description = description;
+  }
+
+  getInterestId(): number {
+    return this.interestId;
+  }
+
+  getInterestName(): string {
+    return this.interestName;
+  }
+
+  getDescription(): string {
+    return this.description;
+  }
+
+}
