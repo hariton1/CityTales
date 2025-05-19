c8dc5f2205d8f9271e7dfc9037248c91a81a4c77
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-19 17:10:47 +0200

    add cors policy in backend and fix imports

--- neo4j-data-access/src/main/java/group_05/ase/neo4j_data_access/Service/Interface/ISearchService.java
+++ neo4j-data-access/src/main/java/group_05/ase/neo4j_data_access/Service/Interface/ISearchService.java
@@ -1,7 +1,7 @@
 package group_05.ase.neo4j_data_access.Service.Interface;
 
+import group_05.ase.neo4j_data_access.DTO.HistoricBuildingDTO;
 import group_05.ase.neo4j_data_access.DTO.HistoricPersonDTO;
-import group_05.ase.neo4j_data_access.DTO.HistoricPlaceDTO;
 
 import java.util.List;
 
@@ -9,5 +9,5 @@ public interface ISearchService {
 
     public List<HistoricPersonDTO> searchPersonsWithKeyword(String keyword);
 
-    public List<HistoricPlaceDTO> searchPlacesWithKeyword(String keyword);
+    public List<HistoricBuildingDTO> searchPlacesWithKeyword(String keyword);
 }
