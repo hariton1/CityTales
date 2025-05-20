7fa834e2536dfd93182f7fe72185b0a3059b66d0
Simon <simon@ripphausen.de>
2025-05-20 12:14:30 +0200

    Added properties for related events, locations and persons to all entity groups in the frontend

--- frontend/src/app/dto/db_entity/HistoricalEventEntity.ts
+++ frontend/src/app/dto/db_entity/HistoricalEventEntity.ts
@@ -1,6 +1,11 @@
 import {EventEntity} from './EventEntity';
+import {HistoricalPersonEntity} from './HistoricalPersonEntity';
+import {HistoricalPlaceEntity} from './HistoricalPlaceEntity';
 
 export interface HistoricalEventEntity {
   content: string;
   person: EventEntity;
+  linkedEvents: HistoricalEventEntity[];
+  linkedPersons: HistoricalPersonEntity[];
+  linkedPlaces: HistoricalPlaceEntity[];
 }
