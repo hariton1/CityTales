733c4de6648e9869d0f5a5d5441f3a9c4b125451
Simon <simon@ripphausen.de>
2025-05-20 12:59:32 +0200

    Added related events, locations, persons service to frontend

--- frontend/src/app/services/person.service.ts
+++ frontend/src/app/services/person.service.ts
@@ -3,6 +3,8 @@ import {Observable} from 'rxjs';
 import {BACKEND_ADDRESS} from '../globals';
 import {Injectable} from '@angular/core';
 import {HistoricalPersonEntity} from '../dto/db_entity/HistoricalPersonEntity';
+import {HistoricalPlaceEntity} from '../dto/db_entity/HistoricalPlaceEntity';
+import {HistoricalEventEntity} from '../dto/db_entity/HistoricalEventEntity';
 
 @Injectable({ providedIn: 'root' })
 export class PersonService {
@@ -22,7 +24,15 @@ export class PersonService {
 
 
   //Get by Vienna History Wiki ID
-  public getLinkedPersonsByVHWikiId(id: number): Observable<HistoricalPersonEntity[]> {
-    return this.httpClient.get<HistoricalPersonEntity[]>(BACKEND_ADDRESS + this.PERSON_PATH + 'links/by/id/' + id);
+  public getLinkedPersons(viennaHistoryWikiId: string): Observable<HistoricalPersonEntity[]> {
+    return this.httpClient.get<HistoricalPersonEntity[]>(BACKEND_ADDRESS + this.PERSON_PATH + 'links/persons/by/id/' + viennaHistoryWikiId);
+  }
+
+  public getLinkedLocations(viennaHistoryWikiId: string): Observable<HistoricalPlaceEntity[]> {
+    return this.httpClient.get<HistoricalPlaceEntity[]>(BACKEND_ADDRESS + this.PERSON_PATH + 'links/buildings/by/id/' + viennaHistoryWikiId);
+  }
+
+  public getLinkedEvents(viennaHistoryWikiId: string): Observable<HistoricalEventEntity[]> {
+    return this.httpClient.get<HistoricalEventEntity[]>(BACKEND_ADDRESS + this.PERSON_PATH + 'links/events/by/id/' + viennaHistoryWikiId);
   }
 }
