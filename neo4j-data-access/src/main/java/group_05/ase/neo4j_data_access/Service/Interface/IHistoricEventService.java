e7399c0bfb612ad8c349c064601f54c249a867ea
Simon <simon@ripphausen.de>
2025-05-20 12:13:40 +0200

    Added endpoints for related persons, events, locations to all entity types Added mapping service to outsource commonly used mapping functions to avoid circular depedencies and duplicate code

--- neo4j-data-access/src/main/java/group_05/ase/neo4j_data_access/Service/Interface/IHistoricEventService.java
+++ neo4j-data-access/src/main/java/group_05/ase/neo4j_data_access/Service/Interface/IHistoricEventService.java
@@ -2,6 +2,7 @@ package group_05.ase.neo4j_data_access.Service.Interface;
 
 import group_05.ase.neo4j_data_access.DTO.HistoricBuildingDTO;
 import group_05.ase.neo4j_data_access.DTO.HistoricEventDTO;
+import group_05.ase.neo4j_data_access.DTO.HistoricPersonDTO;
 
 import java.util.List;
 
@@ -11,4 +12,8 @@ public interface IHistoricEventService {
     List<HistoricEventDTO> getEventByPartialName(String partialName);
 
     List<HistoricEventDTO> getAllLinkedHistoricEventsById(int viennaHistoryWikiId);
+
+    List<HistoricBuildingDTO> getAllLinkedHistoricBuildingsById(int viennaHistoryWikiId);
+
+    List<HistoricPersonDTO> getAllLinkedHistoricPersonsById(int viennaHistoryWikiId);
 }
