5343c4812d1fc36421b3b4311bcc85a645148a58
Bianca <bianca.traeger@tuwien.ac.at>
2025-05-21 10:05:12 +0200

    added test to pipeline

--- authentification/src/test/java/group_05/ase/auth/unit/AppUserRepositoryTest.java
+++ authentification/src/test/java/group_05/ase/auth/unit/AppUserRepositoryTest.java
@@ -1,5 +1,7 @@
-package group_05.ase.auth;
+package group_05.ase.auth.unit;
 
+import group_05.ase.auth.AppUser;
+import group_05.ase.auth.AppUserRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
