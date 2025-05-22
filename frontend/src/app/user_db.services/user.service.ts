4c954f393acedbae733b93e10bb431ae81d10d42
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-22 20:39:24 +0200

    retrieve users from the backend and display in the admin dashboard

---
+++ frontend/src/app/user_db.services/user.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {SERVER_ADDRESS} from '../globals';
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
+import {UserDto} from '../user_db.dto/user.dto';
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+  readonly DOMAIN = SERVER_ADDRESS + 'users';
+
+  constructor(readonly httpClient: HttpClient) {
+  }
+
+  public getAllUsers(): Observable<UserDto[]> {
+    return this.httpClient.get<UserDto[]>(this.DOMAIN)
+      .pipe(
+      map(userList => userList.map(user => {
+        return new UserDto(user.id, user.supabase_id, user.email, user.created_at, user.display_name, user.is_active);
+      }))
+    );
+  }
+
+}
