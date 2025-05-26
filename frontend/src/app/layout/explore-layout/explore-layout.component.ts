49bb296d930a6bac9dc03a9ff02660c194f11568
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-26 14:42:47 +0200

    make explore page responsive

--- frontend/src/app/layout/explore-layout/explore-layout.component.ts
+++ frontend/src/app/layout/explore-layout/explore-layout.component.ts
@@ -1,23 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SidebarComponent} from '../sidebar/sidebar.component';
 import {MapViewComponent} from '../map-view/map-view.component';
 import {HistoricalPlaceEntity} from '../../dto/db_entity/HistoricalPlaceEntity';
 import {BuildingEntity} from '../../dto/db_entity/BuildingEntity';
+import {TuiSegmented} from '@taiga-ui/kit';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-explore-layout',
   imports: [
     SidebarComponent,
-    MapViewComponent
+    MapViewComponent,
+    TuiSegmented,
+    NgIf
   ],
   templateUrl: './explore-layout.component.html',
-  styleUrl: './explore-layout.component.scss'
+  styleUrl: './explore-layout.component.less'
 })
-export class ExploreLayoutComponent {
+export class ExploreLayoutComponent implements OnInit {
+
+  currentViewMobile: 'history' | 'map' = 'history';
+  isMobile = false;
 
   selectedPlace: BuildingEntity | null = null;
   historicalPlaces: BuildingEntity[] = [];
   setDetailedView: boolean = false;
+
+  constructor(readonly breakpointObserver: BreakpointObserver) {
+  }
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe([Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+      });
+  }
 
   setSelectedPlace(place: BuildingEntity) {
     this.selectedPlace = place;
