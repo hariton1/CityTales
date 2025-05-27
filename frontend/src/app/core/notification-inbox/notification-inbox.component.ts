e7ee3727aa7b2f8bf42ba1aea8204422c31bb379
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-05-27 18:41:37 +0200

    create notification inbox component

---
+++ frontend/src/app/core/notification-inbox/notification-inbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-inbox',
+  imports: [],
+  templateUrl: './notification-inbox.component.html',
+  styleUrl: './notification-inbox.component.scss'
+})
+export class NotificationInboxComponent {
+
+}
