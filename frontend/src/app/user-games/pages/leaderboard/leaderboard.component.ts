5b1affa9b146076a5e936d21efe986cdecb623b7
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-28 20:42:11 +0200

    add sample leaderboard

--- frontend/src/app/user-games/pages/leaderboard/leaderboard.component.ts
+++ frontend/src/app/user-games/pages/leaderboard/leaderboard.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import {CommonModule} from '@angular/common';
 @Component({
   selector: 'app-leaderboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.scss'
 })
 export class LeaderboardComponent {
-
+  leaderboard = [
+    { name: 'Zane', points: 950 },
+    { name: 'Alice', points: 920 },
+    { name: 'Yuki', points: 870 },
+    { name: 'Max', points: 810 },
+    { name: 'Nina', points: 790 },
+  ];
 }
