576da9ea8998de4eb038a85b2d0fd3c1824498a6
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-28 21:00:28 +0200

    add toggling

--- frontend/src/app/user-games/pages/leaderboard/leaderboard.component.ts
+++ frontend/src/app/user-games/pages/leaderboard/leaderboard.component.ts
@@ -1,17 +1,45 @@
 import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
+import {TuiSwitch, tuiSwitchOptionsProvider} from '@taiga-ui/kit';
+import {TuiSizeS} from '@taiga-ui/core';
+import {FormsModule} from '@angular/forms';
+
 @Component({
   selector: 'app-leaderboard',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, TuiSwitch, FormsModule],
   templateUrl: './leaderboard.component.html',
-  styleUrl: './leaderboard.component.scss'
+  styleUrls: ['./leaderboard.component.scss'],
+  providers: [
+    tuiSwitchOptionsProvider({showIcons: false, appearance: () => 'primary'}),
+  ],
 })
 export class LeaderboardComponent {
-  leaderboard = [
+  isFriends = false;
+
+  globalLeaderboard = [
     { name: 'Zane', points: 950 },
     { name: 'Alice', points: 920 },
     { name: 'Yuki', points: 870 },
     { name: 'Max', points: 810 },
     { name: 'Nina', points: 790 },
   ];
+
+  friendsLeaderboard = [
+    { name: 'Alice', points: 920 },
+    { name: 'Max', points: 810 },
+    { name: 'Cara', points: 700 },
+  ];
+
+  get leaderboard() {
+    return this.isFriends ? this.friendsLeaderboard : this.globalLeaderboard;
+  }
+
+  toggleLeaderboard(value: boolean) {
+    this.isFriends = value;
+  }
+
+  getSize(isLarge: boolean): TuiSizeS {
+    return isLarge ? 'm' : 's';
+  }
 }
