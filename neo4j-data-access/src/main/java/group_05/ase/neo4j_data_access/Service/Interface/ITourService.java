3c1e8d58d869065b06703043b2076c430a916f37
Simon <e12444081@student.tuwien.ac.at>
2025-05-29 18:46:23 +0200

    Added estimation of trip duration and distance when selecting points

--- neo4j-data-access/src/main/java/group_05/ase/neo4j_data_access/Service/Interface/ITourService.java
+++ neo4j-data-access/src/main/java/group_05/ase/neo4j_data_access/Service/Interface/ITourService.java
@@ -2,7 +2,6 @@ package group_05.ase.neo4j_data_access.Service.Interface;
 
 import group_05.ase.neo4j_data_access.Entity.Tour.TourObject;
 import group_05.ase.neo4j_data_access.Entity.ViennaHistoryWikiBuildingObject;
-import org.springframework.data.neo4j.types.GeographicPoint2d;
 
 import java.util.List;
 import java.util.Map;
@@ -10,8 +9,8 @@ import java.util.Map;
 public interface ITourService {
 
 
-    public TourObject createTour(String name, String description, double start_lat, double start_lng, double end_lat, double end_lng, List<ViennaHistoryWikiBuildingObject> stops, String userId);
-    public Map<String, Double> getDurationDistanceEstimate(double start_lat, double start_lng, double end_lat, double end_lng, List<ViennaHistoryWikiBuildingObject> stops);
-    public TourObject getTourByName(String name);
-    public void deleteTourByName(String name);
+    TourObject createTour(String name, String description, double start_lat, double start_lng, double end_lat, double end_lng, List<ViennaHistoryWikiBuildingObject> stops, String userId);
+    Map<String, Double> getDurationDistanceEstimate(double start_lat, double start_lng, double end_lat, double end_lng, List<ViennaHistoryWikiBuildingObject> stops);
+    TourObject getTourByName(String name);
+    void deleteTourByName(String name);
 }
