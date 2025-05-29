65cb15a2e304af6adc0072d13610f7bfe12ded7f
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-29 15:49:53 +0200

    using combined endpoint to get article data for badges in fe

--- frontend/src/app/user-games/pages/badges/badges.component.ts
+++ frontend/src/app/user-games/pages/badges/badges.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {UserBadgesService} from '../../../user_db.services/user-badges.service';
 import {UserBadgeDTO} from '../../../user_db.dto/user-badge.dto';
 import {CommonModule} from '@angular/common';
+import {CombinedService} from '../../../services/combined.service';
+import {Combined} from '../../../dto/db_entity/Combined';
 
 @Component({
   selector: 'app-badges',
@@ -12,19 +14,27 @@ import {CommonModule} from '@angular/common';
 export class BadgesComponent implements OnInit{
 
   userId: string | null = null;
-  badges: UserBadgeDTO[] = [];
+  badges: (UserBadgeDTO & { articleDetails?: Combined })[] = [];
 
-  constructor(private userBadgeService: UserBadgesService, ) {
+
+  constructor(private userBadgeService: UserBadgesService,private combinedService: CombinedService) {
   }
 
   ngOnInit() {
-    this.userId = '5be46711-4f9c-468b-a6dc-0dce03f3b318';
+    this.retrieveUserID();
     console.log(this.userId);
 
     if (this.userId) {
       this.userBadgeService.getBadgesByUserId(this.userId).subscribe({
         next: (data) => {
-          this.badges = data;
+          this.badges = data.map(badge => {
+            if (badge.earned_at && badge.earned_at.includes(' PM')) {
+              badge.earned_at = badge.earned_at.replace(' PM', '');
+            }
+            return badge;
+          });
+
+          this.enrichBadgesWithArticleDetails();
           console.log(this.badges)
         },
         error: (err) => {
@@ -33,6 +43,22 @@ export class BadgesComponent implements OnInit{
       });
     }
   }
+
+  enrichBadgesWithArticleDetails() {
+    this.badges.forEach((badge, index) => {
+      this.combinedService.getInfosById(badge.article_id).subscribe({
+        next: (articleDetails) => {
+          this.badges[index].articleDetails = articleDetails;
+        },
+        error: (err) => {
+          console.error(`Error loading article details for article ID ${badge.article_id}:`, err);
+        }
+      });
+    });
+
+
+  }
+
   retrieveUserID() {
     const stored = localStorage.getItem("user_uuid");
     if (stored) {
