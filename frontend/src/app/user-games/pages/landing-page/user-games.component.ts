8fbd419410c49fb4c4b69bf3ccf6f2eb834ff4a9
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-29 16:27:38 +0200

    add xp-tracker and unify whole leaderboard

--- frontend/src/app/user-games/pages/landing-page/user-games.component.ts
+++ frontend/src/app/user-games/pages/landing-page/user-games.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {LeaderboardComponent} from '../leaderboard/leaderboard.component';
 import {BadgesComponent} from '../badges/badges.component';
+import {XpTrackerComponent} from '../xp-tracker/xp-tracker.component';
 
 @Component({
   selector: 'app-user-games',
-  imports: [LeaderboardComponent, BadgesComponent],
+  imports: [LeaderboardComponent, BadgesComponent, XpTrackerComponent],
   templateUrl: './user-games.component.html',
   styleUrl: './user-games.component.scss'
 })
