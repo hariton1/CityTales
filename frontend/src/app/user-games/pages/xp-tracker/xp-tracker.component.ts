685ae8c07cbf1ab42c4249ae36059c90448805eb
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-29 15:53:47 +0200

    add empty user xp-tracker component

---
+++ frontend/src/app/user-games/pages/xp-tracker/xp-tracker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xp-tracker',
+  imports: [],
+  templateUrl: './xp-tracker.component.html',
+  styleUrl: './xp-tracker.component.scss'
+})
+export class XpTrackerComponent {
+
+}
