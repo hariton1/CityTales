ad0c0b63f5d5f505d470efcee4acee5cccdb276a
Simon <e12444081@student.tuwien.ac.at>
2025-05-29 17:07:15 +0200

    Added recommended tours to frontend Added map view to tour tab Added Tour composing to frontend Added tour description to frontend

---
+++ frontend/src/app/dto/tour_entity/TourStopEntity.ts
@@ -0,0 +1,5 @@
+import {HistoricalPlaceEntity} from '../db_entity/HistoricalPlaceEntity';
+
+export interface TourStopEntity {
+  stop: HistoricalPlaceEntity;
+}
--- frontend/src/app/layout/tours/tour-layout-component/tour-layout-component.ts
+++ frontend/src/app/layout/tours/tour-layout-component/tour-layout-component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
-import { FormControl} from '@angular/forms';
-import {TuiButton} from '@taiga-ui/core';
+import {Component, ViewChild} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {TuiButton, TuiTextfield} from '@taiga-ui/core';
 import {TuiInputModule} from '@taiga-ui/legacy';
 import {CommonModule} from '@angular/common';
+import {LocationService} from '../../../services/location.service';
+import {TourService} from '../../../services/tour.service';
+import {supabase} from '../../../user-management/supabase.service';
+import {GoogleMap} from '@angular/google-maps';
+import {BuildingEntity} from '../../../dto/db_entity/BuildingEntity';
 
 @Component({
   selector: 'app-tour-layout-component',
   imports: [TuiButton,
-    TuiInputModule, ReactiveFormsModule, CommonModule],
+    TuiInputModule, ReactiveFormsModule, CommonModule, GoogleMap, TuiTextfield],
   templateUrl: './tour-layout-component.html',
   standalone: true,
   styleUrl: './tour-layout-component.scss'
 })
 export class TourLayoutComponent {
+
+  private locationService: LocationService;
+  private tourService: TourService;
+
+  constructor(locationService: LocationService, tourService: TourService) {
+    this.locationService = locationService;
+    this.tourService = tourService;
+  }
+
+
+
   recommendedTours = [
     { name: 'Vienna Highlights', duration: '2 hours', description: 'Visit iconic landmarks in central Vienna.' },
     { name: 'Hidden Gems Walk', duration: '3 hours', description: 'Explore less-known but charming spots.' },
@@ -22,12 +37,131 @@ export class TourLayoutComponent {
 
   tourName = new FormControl('');
   tourDescription = new FormControl('');
+  tourDuration: number = 0;
+  tourDistance: number = 0;
+
 
   createTour(){
-    console.log("Invoked create Tour Button!")
+    var userId: string | undefined = "";
+    this.getUser().then(user => {
+      userId = user.data.session?.access_token
+      console.log(user.data.session);})
+
+    //create tour in backend
+
+    if(!this.startMarker || !this.endMarker){
+      alert("Please select start and end point!");
+      return;
+    }
+
+    this.tourService.createTour(
+      this.tourName.getRawValue()!,
+      this.tourDescription.getRawValue()!,
+      this.startMarker?.getPosition()?.lat()!,
+      this.startMarker?.getPosition()?.lng()!,
+      this.endMarker?.getPosition()?.lat()!,
+      this.endMarker?.getPosition()?.lng()!,
+      this.selectedBuildings,
+      userId!
+    )
+
+
+    }
+
+  async getUser(){
+    return await supabase.auth.getSession();
   }
 
+  //Map logic
 
+  @ViewChild(GoogleMap, { static: false }) map!: GoogleMap;
+
+  zoom = 16;
+  center: google.maps.LatLngLiteral = { lat: 48.19994406631644, lng: 16.371089994357767 };
+  mode: 'start' | 'end' | null = null;
+
+  startMarker: google.maps.Marker | null = null;
+  endMarker: google.maps.Marker | null = null;
+
+  selectedBuildings: any[] = [];
+
+  buildingData: BuildingEntity[] = [];
+
+  markers: google.maps.Marker[] = [];
+
+  ngAfterViewInit() {
+    if (!this.map?.googleMap) {
+      console.error('Google Map is not initialized yet');
+      return;
+    }
+
+    //TODO: replace with user location
+    this.locationService.getLocationsInRadius(48.19994406631644, 16.371089994357767, 500).subscribe(locations => {
+      this.buildingData = locations;
+      console.log(this.buildingData.length);
+
+      const mapRef = this.map.googleMap!;
+      this.buildingData.forEach((b) => {
+        const marker = new google.maps.Marker({
+          position: {"lat": b.latitude, "lng": b.longitude},
+          map: mapRef,
+          title: b.name
+        });
+
+        marker.addListener('click', () => this.addBuildingToRoute(b));
+        this.markers.push(marker);
+
+      });
+      console.log("Markers: "+ this.markers)
+
+    })
+  }
+
+  onMapClick(event: google.maps.MapMouseEvent) {
+    if (!event.latLng) return;
+
+    const mapRef = this.map.googleMap!;
+    const position = event.latLng.toJSON();
+
+    if (this.mode === 'start') {
+      if (this.startMarker) this.startMarker.setMap(null);
+      this.startMarker = new google.maps.Marker({
+        position,
+        map: mapRef,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+        title: 'Start Point'
+      });
+      this.mode = null;
+    } else if (this.mode === 'end') {
+      if (this.endMarker) this.endMarker.setMap(null);
+      this.endMarker = new google.maps.Marker({
+        position,
+        map: mapRef,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+        title: 'End Point'
+      });
+      this.mode = null;
+    }
+  }
+
+  addBuildingToRoute(building: any) {
+      this.selectedBuildings.push(building);
+  }
+
+  clearRoute() {
+    this.selectedBuildings = [];
+    if (this.startMarker) this.startMarker.setMap(null);
+    if (this.endMarker) this.endMarker.setMap(null);
+    this.startMarker = null;
+    this.endMarker = null;
+  }
+
+  deleteStopFromTour(building: any){
+    const index = this.selectedBuildings.indexOf(building);
+    if(index > -1){
+      this.selectedBuildings.splice(index, 1);
+    }
+  }
 
 
 }
