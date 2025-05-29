a8efcf9fd1443d6012a9a96e2a8349db25e39e79
Simon <e12444081@student.tuwien.ac.at>
2025-05-29 10:52:19 +0200

    Added tour component

---
+++ frontend/src/app/layout/tours/tour-layout-component/tour-layout-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-layout-component',
+  imports: [],
+  templateUrl: './tour-layout-component.html',
+  styleUrl: './tour-layout-component.scss'
+})
+export class TourLayoutComponent {
+
+}
