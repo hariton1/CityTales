295447e8d2bbc30cba70f924c064749e16e578e9
Simon <e12444081@student.tuwien.ac.at>
2025-05-30 16:25:16 +0200

    Improved tour.dto.ts in frontend Improved tour.service.ts Added your Tours tab Added lines on map for selected buildings

--- frontend/src/app/dto/tour.dto.ts
+++ frontend/src/app/dto/tour.dto.ts
@@ -1,27 +1,160 @@
 import {UserAccountDto} from './user-account.dto';
+import {BuildingEntity} from './db_entity/BuildingEntity';
+import {TourEntity} from './tour_entity/TourEntity';
 
 export class TourDto {
-  private readonly user: UserAccountDto;
-  private readonly locationIds: string[];
+  private id: number;
+  private name: string;
+  private description: string;
+  private start_lat: number;
+  private start_lng: number;
+  private end_lat: number;
+  private end_lng: number;
+  private stops: BuildingEntity[];
+  private distance: number;
+  private durationEstimate: number;
+  private userId: string;
 
-  constructor(user: UserAccountDto) {
-    this.user = user;
-    this.locationIds = [];
+  constructor(id:number, name: string, description: string, start_lat: number, start_lng: number, end_lat: number, end_lng: number, stops: BuildingEntity[], distance: number, durationEstimate: number, userId: string) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.start_lat = start_lat;
+    this.start_lng = start_lng;
+    this.end_lat = end_lat;
+    this.end_lng = end_lng;
+    this.stops = stops;
+    this.distance = distance;
+    this.durationEstimate = durationEstimate;
+    this.userId = userId;
   }
 
-  addLocation(locationId: string) {
-    this.locationIds.push(locationId);
+  public static fromTourEntity(tourEntity:TourEntity): TourDto {
+    var stops: BuildingEntity[] = JSON.parse(tourEntity.stops);
+    return new TourDto(
+      tourEntity.id,
+      tourEntity.name,
+      tourEntity.description,
+      tourEntity.start_lat,
+      tourEntity.start_lng,
+      tourEntity.end_lat,
+      tourEntity.end_lng,
+      stops,
+      tourEntity.distance,
+      tourEntity.durationEstimate,
+      tourEntity.userId
+    )
   }
 
-  removeLocation(locationId: string) {
-    this.locationIds.splice(this.locationIds.indexOf(locationId), 1);
+  public static ofTourDTo(tourDto:TourDto): TourEntity {
+    var entity: TourEntity = {
+      id: tourDto.getId(),
+      name: tourDto.getName(),
+      description: tourDto.getDescription(),
+      start_lat: tourDto.getStart_lat(),
+      start_lng: tourDto.getStart_lng(),
+      end_lat: tourDto.getEnd_lat(),
+      end_lng: tourDto.getEnd_lng(),
+      stops: JSON.stringify(tourDto.getStops()),
+      distance: tourDto.getDistance(),
+      durationEstimate: tourDto.getDurationEstimate(),
+      userId: tourDto.getUserId()
+    }
+    return entity;
+
   }
 
-  getUser(): UserAccountDto {
-    return this.user;
+
+
+  //setter and getter
+
+  getId(): number {
+    return this.id;
   }
 
-  getLocations(): string[] {
-    return this.locationIds;
+  setId(id: number): void {
+    this.id = id;
   }
+
+  getName(): string {
+    return this.name;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  getDescription(): string {
+    return this.description;
+  }
+
+  setDescription(description: string): void {
+    this.description = description;
+  }
+
+  getStart_lat(): number {
+    return this.start_lat;
+  }
+
+  setStart_lat(start_lat: number): void {
+    this.start_lat = start_lat;
+  }
+
+  getStart_lng(): number {
+    return this.start_lng;
+  }
+
+  setStart_lng(start_lng: number): void {
+    this.start_lng = start_lng;
+  }
+
+  getEnd_lat(): number {
+    return this.end_lat;
+  }
+
+  setEnd_lat(end_lat: number): void {
+    this.end_lat = end_lat;
+  }
+
+  getEnd_lng(): number {
+    return this.end_lng;
+  }
+
+  setEnd_lng(end_lng: number): void {
+    this.end_lng = end_lng;
+  }
+
+  getStops(): BuildingEntity[] {
+    return this.stops;
+  }
+
+  setStops(stops: BuildingEntity[]): void {
+    this.stops = stops;
+  }
+
+  getDistance(): number {
+    return this.distance;
+  }
+
+  setDistance(distance: number): void {
+    this.distance = distance;
+  }
+
+  getDurationEstimate(): number {
+    return this.durationEstimate;
+  }
+
+  setDurationEstimate(durationEstimate: number): void {
+    this.durationEstimate = durationEstimate;
+  }
+
+  getUserId(): string {
+    return this.userId;
+  }
+
+  setUserId(userId: string): void {
+    this.userId = userId;
+  }
+
+
 }
--- frontend/src/app/dto/tour_entity/TourStopEntity.ts
+++
@@ -1,5 +0,0 @@
-import {HistoricalPlaceEntity} from '../db_entity/HistoricalPlaceEntity';
-
-export interface TourStopEntity {
-  stop: HistoricalPlaceEntity;
-}
