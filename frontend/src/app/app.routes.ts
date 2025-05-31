39e6dc7fb790605b78966cb23d29ef2dc19a3a13
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-31 17:18:09 +0200

    add empty friends landing page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import {OnboardingComponent} from './user-interests/onboarding/onboarding.compon
 import {EditInterestsComponent} from './user-interests/edit-interests/edit-interests.component';
 import {FeedbackComponent} from './user-feedback/pages/feedback/feedback.component';
 import {UserGamesComponent} from './user-games/pages/landing-page/user-games.component';
+import {LandingPageComponent} from './user-friends/landing-page/landing-page.component';
 
 export const routes: Routes = [
   {
@@ -32,5 +33,6 @@ export const routes: Routes = [
   { path: 'feedback', component: FeedbackComponent },
   { path: 'edit-interests', component: EditInterestsComponent },
   { path: 'scoreboard', component: UserGamesComponent },
+  { path: 'friends', component: LandingPageComponent },
   { path: '', redirectTo: '/explore', pathMatch: 'full' } //default page, to be changed
 ];
