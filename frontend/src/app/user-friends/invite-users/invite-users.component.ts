e3c9f8e1dc9ed6a93276180d3322a07cabd7a535
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-31 17:48:48 +0200

    add empty user-search and user-inivte components

---
+++ frontend/src/app/user-friends/invite-users/invite-users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-users',
+  imports: [],
+  templateUrl: './invite-users.component.html',
+  styleUrl: './invite-users.component.scss'
+})
+export class InviteUsersComponent {
+
+}
