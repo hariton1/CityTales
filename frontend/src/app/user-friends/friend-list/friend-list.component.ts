2ccaa565f08647413c94dbb6c93563f743821e93
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-05-31 17:20:13 +0200

    add empty friend-list component

---
+++ frontend/src/app/user-friends/friend-list/friend-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-list',
+  imports: [],
+  templateUrl: './friend-list.component.html',
+  styleUrl: './friend-list.component.scss'
+})
+export class FriendListComponent {
+
+}
