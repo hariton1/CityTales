c64aa58d85ec3f1c1c1b2e85d4c2992a9b26b58d
Ivan Lichner <e1226385@student.tuwien.ac.at>
2025-05-31 14:12:47 +0200

    removed success messages and added points earning from opening the detail (visiting, 1pt.)

--- frontend/src/app/layout/historic-place-preview/historic-place-preview.component.ts
+++ frontend/src/app/layout/historic-place-preview/historic-place-preview.component.ts
@@ -5,6 +5,8 @@ import {TuiCardLarge} from '@taiga-ui/layout';
 import {BuildingEntity} from '../../dto/db_entity/BuildingEntity';
 import {UserHistoryDto} from '../../user_db.dto/user-history.dto';
 import {UserHistoriesService} from '../../user_db.services/user-histories.service';
+import {UserPointsService} from '../../user_db.services/user-points.service';
+import {UserPointDto} from '../../user_db.dto/user-point.dto';
 
 
 @Component({
@@ -24,7 +26,8 @@ export class HistoricPlacePreviewComponent {
   @Output() selectPlaceEvent: EventEmitter<BuildingEntity> = new EventEmitter<BuildingEntity>();
   @Output() setDetailedViewEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor(private userHistoriesService: UserHistoriesService) {
+  constructor(private userHistoriesService: UserHistoriesService,
+              private userPointsService: UserPointsService) {
   }
 
   onDetailsClick(place: BuildingEntity) {
@@ -36,16 +39,35 @@ export class HistoricPlacePreviewComponent {
       new Date(0),
       2);
 
+    let newUserPointsEntry = new UserPointDto(
+      -1,
+      "f5599c8c-166b-495c-accc-65addfaa572b",
+      1,
+      new Date()
+    );
+
     this.userHistoriesService.createNewUserHistory(place.userHistoryEntry).subscribe({
       next: (results) => {
         console.log('New user history entry created successfully', results);
         place.userHistoryEntry.setUserHistoryId(results.getUserHistoryId());
-        this.alerts
+        /*this.alerts
           .open('Your new user history entry is saved', {label: 'Success!', appearance: 'success', autoClose: 3000})
-          .subscribe();
+          .subscribe();*/
       },
       error: (err) => {
         console.error('Error creating user history entry:', err);
+      }
+    });
+
+    this.userPointsService.createNewPoints(newUserPointsEntry).subscribe({
+      next: (results) => {
+        console.log('New user points entry created successfully', results);
+        /*this.alerts
+          .open('Your new user history entry is saved', {label: 'Success!', appearance: 'success', autoClose: 3000})
+          .subscribe();*/
+      },
+      error: (err) => {
+        console.error('Error creating user points entry:', err);
       }
     });
 
