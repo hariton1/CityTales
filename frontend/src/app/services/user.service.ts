cda1c57c7d3a30021539f09fa7bae6a71bc00afd
Ivan Lichner <e1226385@student.tuwien.ac.at>
2025-05-31 17:39:26 +0200

    badge and points from notification added

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -210,6 +210,52 @@ export class UserService {
       new Date()
     );
 
+    this.userHistoriesService.createNewUserHistory(node.userHistoryEntry).subscribe({
+      next: (results) => {
+        console.log('New user history entry created successfully', results);
+        node.userHistoryEntry.setUserHistoryId(results.getUserHistoryId());
+        /*this.alerts
+          .open('Your new user history entry is saved', {label: 'Success!', appearance: 'success', autoClose: 3000})
+          .subscribe();*/
+      },
+      error: (err) => {
+        console.error('Error creating user history entry:', err);
+      }
+    });
+
+    this.userPointsService.createNewPoints(newUserPointsEntry).subscribe({
+      next: (results) => {
+        console.log('New user points entry created successfully', results);
+        /*this.alerts
+          .open('Your new user history entry is saved', {label: 'Success!', appearance: 'success', autoClose: 3000})
+          .subscribe();*/
+      },
+      error: (err) => {
+        console.error('Error creating user points entry:', err);
+      }
+    });
+
+    return node;
+  }
+
+  public enterHistoricNodeAlert(node: any): any {
+    console.log(node);
+
+    node.userHistoryEntry = new UserHistoryDto(
+      -1,
+      "f5599c8c-166b-495c-accc-65addfaa572b",
+      Number(node.viennaHistoryWikiId),
+      new Date(),
+      new Date(0),
+      2);
+
+    let newUserPointsEntry = new UserPointDto(
+      -1,
+      "f5599c8c-166b-495c-accc-65addfaa572b",
+      2,
+      new Date()
+    );
+
     let newBadgeEntry = new UserBadgeDTO(
       -1,
       "f5599c8c-166b-495c-accc-65addfaa572b",
