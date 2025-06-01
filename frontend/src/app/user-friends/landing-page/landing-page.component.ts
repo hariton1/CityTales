0af895642ea4e032dcefb7bcecd0c664d8a4da3f
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-06-01 15:22:10 +0200

    add basic user list for sending invites

--- frontend/src/app/user-friends/landing-page/landing-page.component.ts
+++ frontend/src/app/user-friends/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {FriendListComponent} from '../friend-list/friend-list.component';
 import {CommonModule} from '@angular/common';
+import {InviteUsersComponent} from '../invite-users/invite-users.component';
+import {SearchUsersComponent} from '../search-users/search-users.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [FriendListComponent, CommonModule],
+  imports: [FriendListComponent, InviteUsersComponent, SearchUsersComponent, CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
