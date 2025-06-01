cac146080d3fa1cc684f81f0b5c91d4920898dda
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-06-01 15:22:10 +0200

    add basic user list for sending invites

--- frontend/src/app/user-friends/search-users/search-users.component.ts
+++ frontend/src/app/user-friends/search-users/search-users.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../user_db.services/user.service';
+import {UserDto} from '../../user_db.dto/user.dto';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-search-users',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-users.component.html',
   styleUrl: './search-users.component.scss'
 })
-export class SearchUsersComponent {
+export class SearchUsersComponent implements OnInit{
 
+  protected users : UserDto[];
+
+  constructor(private userService: UserService) {
+    this.users = [];
+  }
+  ngOnInit() {
+    this.retrieveUsers();
+  }
+
+  retrieveUsers() {
+    this.userService.getAllUsers()
+      .subscribe({
+        next: (users) => {
+          this.users = users;
+        },
+        error(error) {
+          console.error('Error fetching users:', error);
+        },
+        complete() {
+          console.log('Users fetched successfully!');
+        }
+      });
+  }
+
+  sendFriendInvite(user: UserDto): void {
+    console.log(`Sending friend invite to ${user.email}`);
+    // Implement the real logic here, e.g., via userService.sendFriendInvite(user.id)
+  }
 }
