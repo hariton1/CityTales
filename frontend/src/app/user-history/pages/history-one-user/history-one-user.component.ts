0e5b56ca371fb43e730dc8b08cbc52444d00827a
Ivan Lichner <e1226385@student.tuwien.ac.at>
2025-06-08 15:37:06 +0200

    added empty history page for logged-in user

---
+++ frontend/src/app/user-history/pages/history-one-user/history-one-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-one-user',
+  imports: [],
+  templateUrl: './history-one-user.component.html',
+  styleUrl: './history-one-user.component.scss'
+})
+export class HistoryOneUserComponent {
+
+}
