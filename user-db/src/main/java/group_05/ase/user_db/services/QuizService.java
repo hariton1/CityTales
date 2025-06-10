07ab45b12542dbc520c3443b4d97d643de505995
ChristophWinkler <christophwinkler2@gmail.com>
2025-06-10 04:48:53 +0200

    Add Quiz BE and connect to OpenAI adapter

--- user-db/src/main/java/group_05/ase/user_db/services/QuizService.java
+++ user-db/src/main/java/group_05/ase/user_db/services/QuizService.java
@@ -9,11 +9,12 @@ import group_05.ase.user_db.repositories.QuestionResultsRepository;
 import group_05.ase.user_db.repositories.QuizRepository;
 import group_05.ase.user_db.repositories.QuizUserRepository;
 import group_05.ase.user_db.restData.*;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class QuizService {
@@ -22,6 +23,9 @@ public class QuizService {
     private final QuestionResultsRepository questionResultsRepository;
     private final QuizRepository quizRepository;
     private final QuizUserRepository quizUserRepository;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String OPENAI_ADDRESS = "http://openai-adapter:8088/";
 
     public QuizService(QuestionRepository questionRepository, QuestionResultsRepository questionResultsRepository, QuizRepository quizRepository, QuizUserRepository quizUserRepository) {
         this.questionRepository = questionRepository;
@@ -106,11 +110,32 @@ public class QuizService {
         return results;
     }
 
-    public QuizDTO saveQuiz(QuizDTO quiz) {
-        QuizEntity quizEntity = quizRepository.save(mapToQuizEntity(quiz));
-        List<QuestionEntity> questionEntities = questionRepository.saveAll(mapToQuestionEntityList(quiz.getQuestions()));
+    public QuizDTO saveQuiz(String category, List<UUID> users) {
+        //todo call build creation criteria based on users and category (tours, interests, fun facts ect.)
+        String prompt = "Die Staatsoper war in der Monarchie bis 1918 - wie das Burgtheater - ein Hoftheater, das vom Obersthofmeisteramt des Kaisers beaufsichtigt wurde. Das Hofärar (öffentliche Gelder in der Verwaltung des Kaiserhofes) sorgte für die Finanzierung. In der Republik wird sie als Staatstheater geführt (siehe Bundestheater), die Finanzierung obliegt der Bundesregierung.Bevor das alte Kärntnertortheater 1870 demoliert wurde, ging man an die Erbauung eines neuen Opernhauses, das (über einem Teil des ehemaligen Stadtgrabens, auf den Stadterweiterungsgründen, gelegen) von Eduard van der Nüll und August Sicard von Sicardsburg in romantisch-historisierendem Stil errichtet wurde (Wettbewerbsausschreibung am 10. Juni 1860, Entscheidung der Jury am 28. Oktober 1861, Grundsteinlegung am 20. Mai 1863, Hauptgesimsgleiche am 7. Oktober 1865, Beendigung der Innenausgestaltung im Frühjahr 1869). Nach dem Tod der beiden Architekten 1868 wurde die Hofoper von Gustav Gugitz und Josef Storck vollendet. Das Opernhaus wurde am 25. Mai 1869 mit Mozarts \"Don Giovanni\" eröffnet (Generalprobe 30. April). Bei dem durch einen Fliegerangriff am 12. März 1945 hervorgerufenen Brand wurde die Staatsoper bis auf die Hauptmauern und das große Stiegenhaus am Ring zerstört.In der offenen (heute verglasten) Loggia ober den Haupteingängen Gemälde von Moritz von Schwind, in den fünf Öffnungen allegorischen Bronzefiguren von Ernst Julius Hähnel, auf den beiden Postamenten oberhalb der Loggia je ein Pegasus vom selben Meister (zwei ursprünglich für diesen Platz hergestellte Pegasusfiguren von Vinzenz Pilz erwiesen sich als zu monumental, wurden im April 1870 wieder von ihren Piedestalen entfernt und nach Philadelphia, United States of America, verkauft). Restaurierung der Fassade am Ring ab 2004.Die sieben Statuen im Treppenhaus (Bildhauer Josef Gasser) stellen die sieben freien Künste dar (1869). An der Ausstattung der Räume beteiligten sich die damals bekanntesten Künstler Wiens, wie Carl Rahl, Moritz von Schwind, Johann Preleuthner, Franz Dobiaschofsky, Franz Schönthaler, Hanns und Josef Gasser, Eduard Bitterlich, Christian Griepenkerl, Eduard Engerth, Franz Melnitzky. Die Hochreliefs an der Hinterwand des Stiegenhauses (\"Oper\", \"Ballett\") schuf Johann Baptist Preleuthner , die darunter angebrachten Medaillonreliefs (Nüll, Sicard) Josef Cesar.1905 spendeten die Großindustriellen A. und F. Böhler zwei Kandelaber mit Bronzefiguren, die vor der Oper am Ring aufgestellt wurden. Die eine der beiden Statuen - sie stammten von Fritz Zerritsch, stellte Siegfried dar, das Schwert in der Rechten, in der Linken den Nibelungenring, zu Füßen den getöteten Fafner. Der Held hielt den Kopf horchend erhoben: Es war die Szene, wo er in Wagners Oper der Stimme des Waldvögleins lauschte. Die zweite Figur zeigte den Steinernen Gast mit dem sterbenden Don Juan zu Füßen (Mozart). Beide Figuren wurden während des Zweiten Weltkriegs entfernt, sie sollten eingeschmolzen werden. Nachforschungen über ihr Schicksal ergaben widersprüchliche Angaben: Ein Gewährsmann versichert, er habe beide Statuen 1947 bei einer Firma am Donaukanal gesehen; ihr Metall sei zu schlecht gewesen, um für militärische Zwecke eingeschmolzen zu werden. Später seien sie von dort verschwunden gewesen. Der zweite Gewährsmann behauptet, die beiden Figuren seien gar nicht während des Kriegs abmontiert worden, sondern noch Anfang 1946 vor der Oper gestanden, die russischen Besatzungssoldaten hätten ihre Telephonleitungen an ihnen befestigt, und erst in diesem Jahr seien sie dann verschwunden.Bei dem nach wenigen Jahren einsetzenden Wiederaufbau übernahm Erich Boltenstern die Neugestaltung des Zuschauerraums, der Stiegenaufgänge zwischen den Rängen, der Publikumsgarderoben und der Pausenräume in den oberen Rängen. (Die Deckenbilder des alten Zuschauerraums von Carl Rahl sowie der vom selben Künstler gestaltete Vorhang waren ein Raub der Flammen geworden. Lediglich die Fresken von Eduard Engerth im Kaisersaal konnten teilweise gerettet und später übertragen werden.) Zeno Kosak gestaltete den Gobelinsaal, die Architekten Otto Prossinger und Felix Cewela restaurierten die Seitenfronten und den Marmorsaal, den gesamten Neubau der Bühne besorgte die Bauleitung des Bundesministeriums für Handel und Wiederaufbau in Eigenregie. Rudolf Hermann Eisenmenger schuf die Kartons für die Gobelins im Gobelinsaal sowie die Monumentalmalerei auf dem eisernen Vorhang (Orpheus und Eurydike). Nach Kriegsende wurde der Spielbetrieb im Theater an der Wien (6. Oktober 1945) und in der Volksoper (1. Mai 1945; Figaros Hochzeit) aufgenommen. Die restaurierte Oper wurde am 5. November 1955 mit einer festlichen Aufführung von Beethovens \"Fidelio\" unter Karl Böhm eröffnet. Es war ein gesellschaftliches Ereignis ersten Rangs, aus allen Teilen der Erde kamen prominenteste Gäste nach Wien, um diese Eröffnung mitzuerleben.Seit 1998 erhält der Eiserne Vorhang in jeder Saison ein durch ein besonderes Ereignis (beispielsweise 2003/2004 Parsifal-Jahr) inspiriertes Bild. Es handelt sich dabei um Projekte des \"museum in progress\" in Kooperation mit der Wiener Staatsoper, finanziell ermöglicht durch Sponsoren. Bisher wurden folgende Künstler(innen) beauftragt:Übersiedlung aus dem Hanuschhof (1., Hanuschgasse 3/Goethegasse 1) in die neu adaptierten Räume 1, Operngasse 2 (Eröffnung 14. Februar 2004).Ehrenmitglieder Staatsoper.";
 
-        return mapToQuizDTO(quizEntity, questionEntities);
+        //todo call chatGPT to create a new quiz based on category
+        QuizResponse answer = generateQuizByLLM(prompt);
+
+        //todo save quiz
+        QuizDTO result = new QuizDTO();
+
+        result.setId(100);
+        result.setName("persisted Quiz");
+        result.setDescription(answer.getAnswer());
+        result.setCategory(category);
+        result.setCreator(users.getFirst());
+        result.setQuestions(null);
+        result.setCreatedAt(LocalDateTime.now());
+
+        //QuizEntity quizEntity = quizRepository.save(mapToQuizEntity(quiz));
+        //List<QuestionEntity> questionEntities = questionRepository.saveAll(mapToQuestionEntityList(quiz.getQuestions()));
+        //return mapToQuizDTO(quizEntity, questionEntities);
+
+        //todo add users to quiz via quiz_user table
+
+        //todo return quiz
+        return result;
     }
 
     private QuizDTO mapToQuizDTO(QuizEntity quiz, List<QuestionEntity> questions) {
@@ -200,5 +225,15 @@ public class QuizService {
         }
 
         return result;
+    }
+
+    private QuizResponse generateQuizByLLM(String prompt) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        HttpEntity<String> entity = new HttpEntity<>(prompt, headers);
+        ResponseEntity<QuizResponse> response = restTemplate.exchange(OPENAI_ADDRESS + "/api/quiz/generate", HttpMethod.POST, entity, QuizResponse.class);
+        System.out.println("RESPONSE: " + response);
+        return response.getBody();
     }
 }
