cb9bc26018c8b8175d9050143b1758a3e555e07b
Valentin Kastberger <valentinkastberger98@gmail.com>
2025-06-12 20:09:14 +0200

    bugfix: not showing related persons,events and buildings in orchestrator-filting

--- orchestrator/src/main/java/group_05/ase/orchestrator/dto/ViennaHistoryWikiEventObject.java
+++ orchestrator/src/main/java/group_05/ase/orchestrator/dto/ViennaHistoryWikiEventObject.java
@@ -1,4 +1,5 @@
 package group_05.ase.orchestrator.dto;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,39 +10,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Node("WienGeschichteWikiBuildings")
+@Node("WienGeschichteWikiEvents")
 @Getter
 @Setter
 @ToString
-public class ViennaHistoryWikiBuildingObject {
+public class ViennaHistoryWikiEventObject {
 
     @Id
     private int viennaHistoryWikiId;
     private String url;
     private String name;
 
-    private Optional<String> buildingType = Optional.empty();
+    private Optional<String> typeOfEvent = Optional.empty();
     private Optional<String> dateFrom = Optional.empty();
     private Optional<String> dateTo = Optional.empty();
-    private Optional<String> otherName = Optional.empty();
-    private Optional<String> previousName = Optional.empty();
-    private Optional<String> namedAfter = Optional.empty();
-    private Optional<String> entryNumber = Optional.empty();
-    private Optional<String> architect = Optional.empty();
-    private Optional<String> famousResidents = Optional.empty();
+    private Optional<String> topic = Optional.empty();
+    private Optional<String> organizer = Optional.empty();
+    private Optional<String> participantCount = Optional.empty();
+    private Optional<Boolean> violence = Optional.empty();
     private Optional<String> gnd = Optional.empty();
     private Optional<String> wikidataId = Optional.empty();
     private Optional<String> seeAlso = Optional.empty();
     private Optional<String> resource = Optional.empty();
-    private Optional<Double> latitude = Optional.empty();
-    private Optional<Double> longitude = Optional.empty();
 
     private List<String> links = new ArrayList<>();
     private List<String> imageUrls = new ArrayList<>();
-
-    private List<ViennaHistoryWikiBuildingObject> relatedBuildings = new ArrayList<>();
-    private List<ViennaHistoryWikiPersonObject> relatedPersons = new ArrayList<>();
-    private List<ViennaHistoryWikiEventObject> relatedEvents = new ArrayList<>();
 
     private String contentGerman;
     private String contentEnglish;
