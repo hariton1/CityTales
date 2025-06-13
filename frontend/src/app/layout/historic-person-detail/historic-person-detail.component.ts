be4beba307a7d9889a48b5af480ca167be25e071
Hariton Buçka <e1529018@student.tuwien.ac.at>
2025-06-13 02:19:13 +0200

    apply the new ui to historical persons

--- frontend/src/app/layout/historic-person-detail/historic-person-detail.component.ts
+++ frontend/src/app/layout/historic-person-detail/historic-person-detail.component.ts
@@ -1,6 +1,14 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
-import {TuiIcon, TuiScrollbar} from "@taiga-ui/core";
+import {ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output, signal} from '@angular/core';
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
+import {
+  TuiAppearance,
+  TuiAutoColorPipe,
+  TuiButton,
+  TuiFallbackSrcPipe,
+  TuiIcon,
+  TuiScrollbar, TuiSurface,
+  TuiTitle
+} from "@taiga-ui/core";
 import {HistoricalPersonEntity} from '../../dto/db_entity/HistoricalPersonEntity';
 import {UserHistoriesService} from '../../user_db.services/user-histories.service';
 import {Router} from '@angular/router';
@@ -8,23 +16,79 @@ import {PersonEntity} from '../../dto/db_entity/PersonEntity';
 import {EventEntity} from '../../dto/db_entity/EventEntity';
 import {BuildingEntity} from '../../dto/db_entity/BuildingEntity';
 import {UserService} from '../../services/user.service';
+import {TuiCardCollapsed, TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {TuiExpand} from '@taiga-ui/experimental';
+import {TuiAvatar, TuiCarouselComponent, TuiChevron, TuiPagination, TuiTooltip} from '@taiga-ui/kit';
+import {BreakpointService} from '../../services/breakpoints.service';
+import {EnrichmentService} from '../../services/enrichment.service';
+import {TuiItem} from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-historic-person-detail',
-    imports: [
-        NgForOf,
-        NgIf,
-        TuiIcon,
-        TuiScrollbar
-    ],
+  imports: [
+    NgForOf,
+    NgIf,
+    TuiIcon,
+    TuiScrollbar,
+    TuiButton,
+    TuiCardLarge,
+    TuiExpand,
+    TuiHeader,
+    TuiTitle,
+    TuiChevron,
+    TuiCardCollapsed,
+    AsyncPipe,
+    TuiAppearance,
+    TuiAutoColorPipe,
+    TuiAvatar,
+    TuiCarouselComponent,
+    TuiFallbackSrcPipe,
+    TuiPagination,
+    TuiSurface,
+    TuiTooltip,
+    TuiItem
+  ],
   templateUrl: './historic-person-detail.component.html',
-  styleUrl: './historic-person-detail.component.scss'
+  styleUrl: './historic-person-detail.component.less'
 })
-export class HistoricPersonDetailComponent {
+export class HistoricPersonDetailComponent implements OnInit{
 
-  constructor(private router: Router, private userService: UserService) {
+  protected index1 = 0; //tone slider
+  lineWidths = [90, 70, 95, 60, 85, 80, 60, 75, 85, 80];
+  isMobile = false;
+
+  customBreakpointLevel: CustomBreakpointLevel = null;
+
+  summary: string = '';
+  enrichedContent: string = '';
+  enrichmentStarted = false;
+  enrichmentLoading = false;
+  tonesItemCount = 3;
+
+  public readonly collapsed = signal(true);
+
+  constructor(private router: Router,
+              private userService: UserService,
+              readonly EnrichmentService: EnrichmentService,
+              readonly cdr: ChangeDetectorRef,
+              private breakpointService: BreakpointService) {
   }
 
+  tones = [
+    { key: 'academic', label: 'Historian', bg: 'historian-bg.png' },
+    { key: 'tour', label: 'Tour Guide', bg: 'tour-guide-bg.png' },
+    { key: 'poetic', label: 'Poetic', bg: 'poetic-bg.png' },
+    { key: 'dramatic', label: 'Dramatic', bg: 'dramatic-bg.png' },
+    { key: 'child-friendly', label: 'Child-Friendly', bg: 'child-friendly-bg.png' },
+    { key: 'funny', label: 'Funny', bg: 'funny-bg.jpg' },
+  ];
+
+  ngOnInit() {
+    this.breakpointService.level$.subscribe(() => {
+      this.tonesItemCount = this.breakpointService.tonesItemCount;
+      this.cdr.detectChanges();
+    });
+  }
 
   @Input()
   get selectedPerson(): any {
@@ -68,4 +132,85 @@ export class HistoricPersonDetailComponent {
       }
     });
   }
+
+  startEnrichment(tone: string): void {
+    this.enrichmentStarted = true;
+    this.enrichmentLoading = true;
+
+    let content = this.selectedPerson.contentGerman;
+
+    this.EnrichmentService.enrichContentWithTone(tone, content).subscribe({
+      next: (response) => {
+        console.log('tone: ' + response.tone);
+        console.log('summary: ' + response.summary);
+        console.log('enrichedContent: ' + response.enrichedContent);
+
+        this.summary = response.summary;
+        this.enrichedContent = response.enrichedContent;
+      },
+      error: (error: any) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log('Completed');
+        this.enrichmentLoading = false;
+        this.cdr.detectChanges();
+      }
+    })
+  }
+
+  protected readonly itemsCount = 3;
+  protected index2 = 0; //related persons slider
+  protected index3 = 0; //related buildings slider
+  protected index4 = 0; //related events slider
+
+  protected get roundedPersons(): number {
+    return Math.floor(this.index2 / this.itemsCount);
+  }
+
+  protected onIndexPersons(index: number): void {
+    this.index2 = index * this.itemsCount;
+    this.cdr.detectChanges();
+  }
+
+  protected get roundedBuildings(): number {
+    return Math.floor(this.index3 / this.itemsCount);
+  }
+
+  protected onIndexBuildings(index: number): void {
+    this.index3 = index * this.itemsCount;
+    this.cdr.detectChanges();
+  }
+
+  protected get roundedEvents(): number {
+    return Math.floor(this.index4 / this.itemsCount);
+  }
+
+  protected onIndexEvents(index: number): void {
+    this.index4 = index * this.itemsCount;
+    this.cdr.detectChanges();
+  }
+
+  get personsPageCount(): number {
+    return Math.ceil(this.selectedPerson?.relatedPersons?.length / this.itemsCount);
+  }
+
+  get buildingsPageCount(): number {
+    return Math.ceil(this.selectedPerson?.relatedBuildings?.length / this.itemsCount);
+  }
+
+  get eventsPageCount(): number {
+    return Math.ceil(this.selectedPerson?.relatedEvents?.length / this.itemsCount);
+  }
 }
+
+type CustomBreakpointLevel =
+  | 'mobile'          // 360–767px
+  | 'tablet'          // 768–1023px
+  | 'desktop'         // 1024–1279px
+  | '1280-1499'
+  | '1500-1899'
+  | '1900-2559'
+  | '2560+'
+  | 'unknown'
+  | null;
