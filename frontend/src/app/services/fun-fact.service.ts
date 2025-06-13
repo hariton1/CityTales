39ff96d1d1997faadd852dd597e4be36498cd845
Bianca <bianca.traeger@tuwien.ac.at>
2025-06-13 20:28:23 +0200

    added fun fact to detail view place

---
+++ frontend/src/app/services/fun-fact.service.ts
@@ -0,0 +1,42 @@
+// src/app/services/fun-fact.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+
+export interface FunFactCardDTO {
+  id: number;
+  fact: string;
+  score: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FunFactService {
+  private apiUrl = '/api/funfact';
+  private useDummy = true;
+
+  constructor(private http: HttpClient) {}
+
+  getBuildingFunFact(buildingId: number): Observable<FunFactCardDTO> {
+    if (this.useDummy) {
+      // Hier kommt immer derselbe Dummy zurück (du kannst ihn beliebig anpassen)
+      return of({
+        id: buildingId,
+        fact: `Dies ist ein Dummy-Fun-Fact für Gebäude #${buildingId}.`,
+        score: Math.floor(Math.random() * 100)
+      });
+    } else {
+      // Hier kommt später dein echter HTTP-Call hin!
+      // return this.http.get<FunFactCardDTO>(`${this.apiUrl}/building/${buildingId}`);
+      return this.http.get<FunFactCardDTO>(`${this.apiUrl}/building/${buildingId}`);
+    }
+
+  }
+  getPersonFunFact(personId: number): Observable<FunFactCardDTO> {
+    return this.http.get<FunFactCardDTO>(`${this.apiUrl}/person/${personId}`);
+  }
+  getEventFunFact(eventId: number): Observable<FunFactCardDTO> {
+    return this.http.get<FunFactCardDTO>(`${this.apiUrl}/event/${eventId}`);
+  }
+}
