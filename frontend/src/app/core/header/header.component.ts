9e46fb5c20b6350f0a494acc7f06693097132a0d
Ivan Lichner <e1226385@student.tuwien.ac.at>
2025-06-16 10:50:28 +0200

    bug fixing - added restriction to displaying Dashboard link in navigation menu based on UserRole (for now refresh needed after logging in)

--- frontend/src/app/core/header/header.component.ts
+++ frontend/src/app/core/header/header.component.ts
@@ -1,15 +1,15 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, inject} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
 import {TuiButton, TuiDataListComponent, TuiDropdown, TuiIcon, TuiSizeL, TuiSizeS} from '@taiga-ui/core';
 import {TuiDataListDropdownManager, TuiSegmented, TuiSwitch} from '@taiga-ui/kit';
 
 import {TuiHeaderComponent, TuiLogoComponent} from '@taiga-ui/layout';
-import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 
 import {SearchService} from '../../services/search.service'
 import {BuildingEntity} from '../../dto/db_entity/BuildingEntity';
 import { supabase } from '../../user-management/supabase.service';
 import { CommonModule } from '@angular/common';
-import {TuiPlatform, TuiThemeColorService} from '@taiga-ui/cdk';
+import {TuiPlatform} from '@taiga-ui/cdk';
 import {FormsModule} from '@angular/forms';
 import { Inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
@@ -26,7 +26,6 @@ import { isPlatformBrowser } from '@angular/common';
     TuiHeaderComponent,
     TuiIcon,
     RouterLink,
-    RouterLinkActive,
     TuiDropdown,
     TuiDataListDropdownManager,
     TuiDataListComponent,
@@ -44,6 +43,7 @@ export class HeaderComponent {
   protected open = false;
   protected size: TuiSizeL | TuiSizeS = 'l';
   protected isChecked = true;
+  protected isAdmin = false;
   protected readonly platforms: ReadonlyArray<'android' | 'ios' | 'web'> = [
     'web'
   ];
@@ -58,10 +58,18 @@ export class HeaderComponent {
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private searchService: SearchService, private router: Router, private cdr: ChangeDetectorRef) {
     supabase.auth.getSession().then(({ data: { session } }) => {
       this.loggedIn = !!session;
+      if(session) {
+        const userRole = localStorage.getItem('user_role');
+        this.isAdmin = userRole === 'Admin' || userRole === 'Moderator';
+      }
       this.cdr.markForCheck();
     });
     supabase.auth.onAuthStateChange((_event, session) => {
       this.loggedIn = !!session;
+      if(session) {
+        const userRole = localStorage.getItem('user_role');
+        this.isAdmin = userRole === 'Admin' || userRole === 'Moderator';
+      }
       this.cdr.markForCheck();
     });
     this.isBrowser = isPlatformBrowser(this.platformId);
