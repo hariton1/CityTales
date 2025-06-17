88e558131494fc61b492b6588dfabee54050074a
Ivan Lichner <e1226385@student.tuwien.ac.at>
2025-06-17 15:56:01 +0200

    added logging into Java modules

--- neo4j-data-access/src/main/java/group_05/ase/neo4j_data_access/Controller/CombinedController.java
+++ neo4j-data-access/src/main/java/group_05/ase/neo4j_data_access/Controller/CombinedController.java
@@ -2,6 +2,8 @@ package group_05.ase.neo4j_data_access.Controller;
 
 import group_05.ase.neo4j_data_access.Entity.CombinedObject;
 import group_05.ase.neo4j_data_access.Service.Implementation.CombinedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ public class CombinedController {
 
     private final CombinedService combinedService;
 
+    private static final Logger logger = LoggerFactory.getLogger(CombinedController.class);
+
     public CombinedController(CombinedService combinedService) {
         this.combinedService = combinedService;
     }
@@ -21,10 +25,11 @@ public class CombinedController {
     @GetMapping("/id/{viennaHistoryWikiId}")
     public ResponseEntity<CombinedObject> getCombinedObjectById(@PathVariable int viennaHistoryWikiId) {
         CombinedObject combinedDto = combinedService.getCombinedObjectById(viennaHistoryWikiId) ;
-
+        logger.info("Fetching combined object with id: {}", viennaHistoryWikiId);
         if (combinedDto != null) {
             return ResponseEntity.ok(combinedDto);
         } else {
+            logger.error("Combined object with id {} not found", viennaHistoryWikiId);
             return ResponseEntity.notFound().build();
         }
     }
